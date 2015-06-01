4348823ca4498825bc5081db5223084ee2fc40e0
Nik Butenko <nik@butenko.me>
2015-06-01 12:32:10 +1000

    Support nodejs (use ES5 for dev server)

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 'use strict';
 
-let path = require('path');
-let webpack = require('webpack');
-let WebpackDevServer = require('webpack-dev-server');
-let config = require('./webpack.config.js').development;
 
-let server = new WebpackDevServer(webpack(config), {
+var path = require('path');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config.js').development;
+
+
+var server = new WebpackDevServer(webpack(config), {
   contentBase: 'src/static/',
   stats: {
     // Do not show list of hundreds of files included in a bundle
@@ -16,7 +18,12 @@ let server = new WebpackDevServer(webpack(config), {
   hot: true
 });
 
-server.use('/', (req, res) => res.sendFile(path.join(__dirname, '/src/static/index.html')));
 
-server.listen(3000, 'localhost', err =>
-err ? console.error(err) : console.log('Listening on http://localhost:3000'));
+server.use('/', function (req, res) {
+  return res.sendFile(path.join(__dirname, '/src/static/index.html'));
+});
+
+
+server.listen(3000, 'localhost', function (err) {
+  return err ? console.error(err) : console.log('Listening on http://localhost:3000');
+});
